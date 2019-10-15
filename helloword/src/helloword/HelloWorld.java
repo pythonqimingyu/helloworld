09dfb96373768b1ce361852054f6da7ef8969371
Administrator <Administrator@10.5.5.110>
2019-10-15 13:49:57 +0800

    test

---
+++ helloword/src/helloword/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package helloword;
+
+/**
+ * @version:HelloWorld - 1.0
+ * @Description:
+ * @author: QiQi
+ * @date: 2019年10月15日
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ helloword/src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/** 
+* @version:module-info - 1.0 
+* @Description: 
+* @author: QiQi 
+* @date: 2019年10月15日
+*/
+module helloword {
+}
